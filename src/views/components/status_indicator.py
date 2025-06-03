88fa9e5f79645e39cd81cd59e6c80b20395bf04b
disjoint7 <disjoint7@163.com>
2025-06-03 15:24:54 +0800

    移除无用代码

--- src/views/components/status_indicator.py
+++ src/views/components/status_indicator.py
@@ -1,6 +1,5 @@
 from enum import Enum, auto
 
-from nicegui import ui
 
 
 class Status(Enum):
@@ -9,25 +8,3 @@ class Status(Enum):
     SUCCEEDED = auto()
     FAILED = auto()
 
-
-class StatusIndicator:
-    def __init__(self, target_object, target_name):
-        self._label = ui.label().bind_text_from(
-            target_object,
-            target_name,
-            backward=lambda s: StatusIndicator._text_backward(s),
-        )
-
-    def label(self):
-        return self._label
-
-    @staticmethod
-    def _text_backward(status: Status) -> str:
-        if status == Status.PENDING:
-            return "🟡"
-        elif status == Status.RUNNING:
-            return "👀"
-        elif status == Status.SUCCEEDED:
-            return "✅"
-        elif status == Status.FAILED:
-            return "❌"
